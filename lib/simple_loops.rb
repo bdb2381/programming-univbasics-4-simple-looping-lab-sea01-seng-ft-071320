fc8b5b41f1c227aaa65cc8a7829c401b5de7ca09
Brad Beggs <brad.beggs@gmail.com>
2020-06-26 19:44:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,19 +13,9 @@ end
 
 
 def output_array(array)
-
-  #counter =
   n = 0
    array.size.times do
       puts array[n]
       n+=1
-  end
-
-=begin
-#n = 0
-#while n < array.size
-#  puts array[n]
-#  n += 1
-#  end
-=end
+    end
 end
